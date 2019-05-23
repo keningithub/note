0083e9a07a3399d84bae3e714d5dcf297e69f4dc
zhengjianqiang@kingsoft.com <zhengjianqiang@kingsoft.com>
2019-05-23 10:20:38 +0800

    update

---
+++ English/日积月累.mm
@@ -0,0 +1,131 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1551669368756" ID="ID_1953628148" MODIFIED="1554258914762" TEXT="&#x65e5;&#x79ef;&#x6708;&#x7d2f;">
+<node CREATED="1552984369305" ID="ID_1471421603" MODIFIED="1552984375513" POSITION="left" TEXT="&#x8bcd;&#x6c47;">
+<node CREATED="1552984067624" ID="ID_507587468" MODIFIED="1552984365350" TEXT="be referred to as">
+<node CREATED="1552984109606" ID="ID_45430175" MODIFIED="1552984119664" TEXT="&#x88ab;&#x79f0;&#x4e3a;"/>
+</node>
+<node CREATED="1552984310700" ID="ID_1706308342" MODIFIED="1552984340272" TEXT="in general">
+<node CREATED="1552984340273" ID="ID_1165126735" MODIFIED="1552984361165" TEXT="&#x603b;&#x4e4b;, &#x4e00;&#x822c;&#x6765;&#x8bf4;"/>
+</node>
+<node CREATED="1552984491415" ID="ID_1086607102" MODIFIED="1552984501949" TEXT="rather than">
+<node CREATED="1552984524254" ID="ID_372120910" MODIFIED="1552984528994" TEXT="&#x800c;&#x4e0d;&#x662f;"/>
+</node>
+<node CREATED="1552986015279" ID="ID_1843118569" MODIFIED="1552986032099" TEXT="and can then">
+<node CREATED="1552986033326" ID="ID_327768703" MODIFIED="1552986038971" TEXT="&#x7136;&#x540e;&#x53ef;&#x4ee5;"/>
+</node>
+<node CREATED="1553228902291" ID="ID_546641177" MODIFIED="1553228910072" TEXT="everywhere">
+<node CREATED="1553228910979" ID="ID_1171272663" MODIFIED="1553228926065" TEXT="&#x5230;&#x5904;, &#x6bcf;&#x4e2a;&#x5730;&#x65b9;"/>
+</node>
+<node CREATED="1553229309384" ID="ID_894744816" MODIFIED="1553229310730" TEXT="but until then">
+<node CREATED="1553229310730" ID="ID_723156300" MODIFIED="1553229326678" TEXT="&#x4f46;&#x76f4;&#x5230;&#x73b0;&#x5728;, &#x4f46;&#x5728;&#x6b64;&#x4e4b;&#x524d;"/>
+</node>
+<node CREATED="1557817851733" ID="ID_616913760" MODIFIED="1557817857213" TEXT="as with">
+<node CREATED="1557817857214" ID="ID_1619605325" MODIFIED="1557817866843" TEXT="&#x6b63;&#x5982;, &#x4e0e;...&#x4e00;&#x6837;"/>
+</node>
+<node CREATED="1557820152673" ID="ID_541587398" MODIFIED="1557820173577" TEXT="as soon as">
+<node CREATED="1557820173578" ID="ID_1175062133" MODIFIED="1557820202212" TEXT="&#x9a6c;&#x4e0a;, &#x4e00;...&#x5c31;"/>
+</node>
+<node CREATED="1557845038561" ID="ID_1927587179" MODIFIED="1558080277312" TEXT="before hand">
+<node CREATED="1557845044930" ID="ID_1016633284" MODIFIED="1557845058767" TEXT="&#x4e8b;&#x5148;, &#x9884;&#x5148;"/>
+</node>
+<node CREATED="1558080277717" ID="ID_568342385" MODIFIED="1558080325415" TEXT="[prevent|stop|keep] sb from doing sth">
+<node CREATED="1558080328241" ID="ID_599287266" MODIFIED="1558342201191" TEXT="&#x963b;&#x6b62;|&#x9632;&#x6b62;&#x67d0;&#x4eba;&#x505a;&#x67d0;&#x4e8b;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#19979;&#38754;&#20004;&#20010;&#32467;&#26500;&#37117;&#21487; &#20197;&#29992;&#65288;&#27809;&#26377;&#21306;&#21035;&#65289;&#65306;<br style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none" /></font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">prevent sb (from) doing sth &#65288;from&#21487;&#29992;&#20063;&#21487;&#19981;&#29992;&#65289;</font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">prevent sb's doing sth &#65288;&#19981;&#33021;&#29992;from&#65289;</font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#25152;&#20197;&#20320;&#30340;&#21477;&#23376;&#65306;<br style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none" /></font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#27491;&#65306;Nothing would&#160;</font><font color="rgb(192, 0, 0)" face="arial, helvetica, sans-serif">prevent him from speaking</font><font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#160;out against injustice.</font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#27491;&#65306;Nothing would </font><font color="rgb(192, 0, 0)" face="arial, helvetica, sans-serif">prevent him speaking</font><font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#160;out against injustice.</font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#27491;&#65306;Nothing would&#160;</font><font color="rgb(192, 0, 0)" face="arial, helvetica, sans-serif">prevent his speaking</font><font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#160;out against injustice.</font>
+    </p>
+    <p style="margin-top: 0px; margin-right: 0px; margin-bottom: 10px; margin-left: 0px; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; list-style-type: none; font-size: 14px; color: rgb(0, 0, 0); font-family: Helvetica Neue, Helvetica, sans-serif; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#35823;&#65306;Nothing would&#160;</font><font color="rgb(192, 0, 0)" face="arial, helvetica, sans-serif">prevent from his speaking</font><font color="rgb(0, 0, 0)" face="arial, helvetica, sans-serif">&#160;out against injustice.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1558341506329" ID="ID_1619210276" MODIFIED="1558341552356" TEXT="&#x9664;keep&#x5916;, prevent/stop&#x4e2d;&#x7684;from&#x53ef;&#x4ee5;&#x7701;&#x7565;">
+<node CREATED="1558341555707" ID="ID_64567575" MODIFIED="1558341579561" TEXT="keep sb doing: &#x8ba9;&#x67d0;&#x4eba;&#x4e00;&#x76f4;...."/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1553828009991" ID="ID_26909055" MODIFIED="1554259058085" POSITION="left" TEXT="strange pronunciation">
+<node CREATED="1553828033302" ID="ID_1504793978" MODIFIED="1553828036410" TEXT="composite">
+<node CREATED="1553828036411" ID="ID_1518507359" MODIFIED="1553828049423" TEXT="/&apos;k&#x252;mp&#x259;z&#x26a;t/"/>
+</node>
+<node CREATED="1554109102843" ID="ID_1428822842" MODIFIED="1554109109648" TEXT="pseudo">
+<node CREATED="1554109129964" ID="ID_889655331" MODIFIED="1554109135057" TEXT="/&apos;sju&#x2d0;d&#x259;&#x28a;/"/>
+</node>
+<node CREATED="1554258452227" ID="ID_643338434" MODIFIED="1554258471524" TEXT="discipline">
+<node CREATED="1554258471526" ID="ID_532474790" MODIFIED="1554258500022" TEXT="/&apos;d&#x26a;s&#x26a;pl&#x26a;n/ "/>
+</node>
+<node CREATED="1554258795367" ID="ID_543995197" MODIFIED="1554258809497" TEXT="analyze">
+<node CREATED="1554258809498" ID="ID_323717202" MODIFIED="1554258833119" TEXT="/&apos;&#xe6;n&#x259;,la&#x26a;z/"/>
+<node CREATED="1554258834303" ID="ID_970141824" MODIFIED="1554258847970" TEXT="analysis">
+<node CREATED="1554258847971" ID="ID_1575498495" MODIFIED="1554258889207" TEXT=" /&#x259;&apos;n&#xe6;l&#x26a;s&#x26a;s/ "/>
+</node>
+</node>
+<node CREATED="1557924868381" ID="ID_828810619" MODIFIED="1557924875610" TEXT="favorite">
+<node CREATED="1557924877428" ID="ID_581619233" MODIFIED="1557924889604" TEXT="/&apos;fe&#x26a;v&#x259;r&#x26a;t/"/>
+</node>
+</node>
+<node CREATED="1557992069903" ID="ID_1819865421" MODIFIED="1557992180076" POSITION="right" TEXT="similarity">
+<node CREATED="1551669379967" ID="ID_1843415408" MODIFIED="1551669488161" TEXT="organize / recognize"/>
+<node CREATED="1551671202825" ID="ID_1835912025" MODIFIED="1551671355029" TEXT="complain / explain"/>
+<node CREATED="1551681833068" ID="ID_1038386961" MODIFIED="1551681841103" TEXT="certain / some">
+<node CREATED="1551688239270" ID="ID_1868057236" MODIFIED="1551688247013" TEXT="certain">
+<node CREATED="1551688247014" ID="ID_25824057" MODIFIED="1551688286907" TEXT="&#x901a;&#x5e38;&#x4e0e;&#x51a0;&#x8bcd;&#x4e00;&#x8d77;&#x4f7f;&#x7528;">
+<node CREATED="1551688286909" ID="ID_500321980" MODIFIED="1551688308846" TEXT="a certain reason"/>
+</node>
+</node>
+<node CREATED="1551688310904" ID="ID_552443866" MODIFIED="1551688314873" TEXT="some">
+<node CREATED="1551688314874" ID="ID_798901827" MODIFIED="1551688348776" TEXT="&#x901a;&#x5e38;&#x4e0d;&#x4e0e;&#x51a0;&#x8bcd;&#x8fde;&#x7528;">
+<node CREATED="1551688348777" ID="ID_591414386" MODIFIED="1551688359242" TEXT="some reason"/>
+</node>
+</node>
+</node>
+<node CREATED="1552530653505" ID="ID_69899134" MODIFIED="1552530666080" TEXT="yield">
+<node CREATED="1552530676046" ID="ID_919221336" MODIFIED="1552530920308" TEXT="&#x5c48;&#x670d; &gt; &#x6295;&#x964d; &gt; &#x7f34;&#x51fa;(&#x6b66;&#x5668;) &gt; &#x4ea7;&#x51fa;"/>
+</node>
+<node CREATED="1552983161795" ID="ID_1422383638" MODIFIED="1552983195529" TEXT="special / particular">
+<node CREATED="1552983195530" ID="ID_942574295" MODIFIED="1552983228786" TEXT="special: &#x7279;&#x522b;&#x7684;, &#x4e13;&#x95e8;&#x7684;"/>
+<node CREATED="1552983229525" ID="ID_522735394" MODIFIED="1552983253265" TEXT="particular: &#x7279;&#x522b;&#x7684;, &#x8be6;&#x7ec6;&#x7684;, &#x7ec6;&#x8282;&#x7684;"/>
+</node>
+<node CREATED="1552984704191" ID="ID_727794257" MODIFIED="1552984825060" TEXT="straight / strange">
+<node CREATED="1557994337099" ID="ID_912533756" MODIFIED="1557994399786" TEXT="&#x76f4;&#x7684;, &#x7b14;&#x76f4;&#x7684; / &#x5947;&#x602a;&#x7684;, &#x964c;&#x751f;&#x7684;"/>
+</node>
+<node CREATED="1557992199942" ID="ID_1380542824" MODIFIED="1557992411554" TEXT="sweet / sour / bitter / spicy / salty">
+<node CREATED="1557994262614" ID="ID_932851509" MODIFIED="1557994289769" TEXT="&#x751c; / &#x9178; / &#x82e6; / &#x8fa3; / &#x54b8;"/>
+</node>
+<node CREATED="1557994221995" ID="ID_1850679333" MODIFIED="1558443481841" TEXT="diffuse / specular">
+<node CREATED="1557994241568" ID="ID_1296596072" MODIFIED="1557994257347" TEXT="&#x6f2b;&#x5c04; / &#x53cd;&#x5c04;"/>
+</node>
+<node CREATED="1558443483102" ID="ID_343801224" MODIFIED="1558507305126" TEXT="parallel / concurrent">
+<node CREATED="1558443499186" ID="ID_1327127931" MODIFIED="1558443508244" TEXT="&#x5e76;&#x884c; / &#x5e76;&#x53d1;"/>
+</node>
+</node>
+</node>
+</map>
