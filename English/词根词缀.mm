b8df240f20bcf4822e3fed6664c4a2f17e3c94f9
zhengjianqiang@kingsoft.com <zhengjianqiang@kingsoft.com>
2019-05-23 10:19:38 +0800

    update

---
+++ English/词根词缀.mm
@@ -0,0 +1,56 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1551669247806" ID="ID_1450738826" MODIFIED="1551669314145" TEXT="&#x8bcd;&#x6839;&#x8bcd;&#x7f00;">
+<node CREATED="1551155121356" ID="ID_725350364" MODIFIED="1551155126900" POSITION="right" TEXT="&#x8bcd;&#x6839;">
+<node CREATED="1552373413514" ID="ID_642865139" MODIFIED="1552373418574" TEXT="matter">
+<node CREATED="1552373419529" ID="ID_1200914891" MODIFIED="1552373445640" TEXT="material">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      matter + i + al
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1551155130000" ID="ID_1093827873" MODIFIED="1551155137709" POSITION="right" TEXT="&#x8bcd;&#x7f00;">
+<node CREATED="1551155138529" ID="ID_1226454875" MODIFIED="1551155143652" TEXT="&#x524d;&#x7f00;">
+<node CREATED="1551155150743" ID="ID_757676698" MODIFIED="1551155409390" TEXT="su-">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(34, 34, 34)" face="Georgia, Garamond, Times New Roman, Times, Microsoft YaHei, serif" size="21px">&#19979;,&#20302;;&#30001;&#19979;&#21521;&#19978;</font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1551155305133" ID="ID_1655170301" MODIFIED="1551155310250" TEXT="suc-">
+<node CREATED="1551155426253" ID="ID_495971475" MODIFIED="1552270859325" TEXT="success">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      suc-: &#19979;, &#20174;&#19979;&#24448;&#19978; + -cess-: &#36208; &gt; &#36208;&#21040;&#26368;&#21518;&#23601;&#26159;&#25104;&#21151;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1551155311284" ID="ID_1417961566" MODIFIED="1551155315633" TEXT="sus-"/>
+<node CREATED="1551155318989" ID="ID_1747295555" MODIFIED="1551155321321" TEXT="sup-"/>
+<node CREATED="1551155321851" ID="ID_276712376" MODIFIED="1551155328424" TEXT="sug-"/>
+<node CREATED="1551155329387" ID="ID_1064304907" MODIFIED="1551155332104" TEXT="suf-"/>
+</node>
+</node>
+<node CREATED="1551155144247" ID="ID_989323409" MODIFIED="1551155146957" TEXT="&#x540e;&#x7f00;"/>
+</node>
+</node>
+</map>
